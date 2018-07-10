13b5e75980f82390d79b27a2b352c775da8e4515
learning <learning@DIN66009008.corp.capgemini.com>
2018-07-10 10:15:47 +0530

    1 version

---
+++ mywallet/src/main/java/com/cg/mywallet/mywallet/App.java
@@ -0,0 +1,14 @@
+package com.cg.mywallet.mywallet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "VARUN" );
+        System.out.println("fsdgd");
+    }
+}
